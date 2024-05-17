0f116de9b97b334db9d1aa786cd589848ad87b3e
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-05-18 02:32:37 +0300

    GO

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,94 +1,97 @@
-import {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updContact,
-} from "../services/contactsServices.js";
-import {
-  createContactSchema,
-  updateContactSchema,
-} from "../schemas/contactsSchemas.js";
-import crypto from "node:crypto";
+import { Contact } from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {
-  listContacts()
-    .then((contacts) => res.status(200).json(contacts))
-    .catch((err) => res.status(500).json({ message: err.message }));
-};
+export async function getAllContacts(req, res) {
+  try {
+    const result = await Contact.find();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
-export const getOneContact = (req, res) => {
-  getContactById(req.params.id)
-    .then((contact) => {
-      if (contact) {
-        res.status(200).json(contact);
-      } else {
-        res.status(404).json({ message: "Not found" });
-      }
-    })
-    .catch((err) => res.status(500).json({ message: err.message }));
-};
+export async function getOneContact(req, res) {
+  const { id } = req.params;
+  try {
+    const result = await Contact.findById(id);
+    if (result === null) {
+      return res.status(404).send({ message: "Not found" });
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
-export const deleteContact = (req, res) => {
-  removeContact(req.params.id)
-    .then((contact) => {
-      if (contact) {
-        res.status(200).json(contact);
-      } else {
-        res.status(404).json({ message: "Not found" });
-      }
-    })
-    .catch((err) => res.status(500).json({ message: err.message }));
-};
+export async function deleteContact(req, res) {
+  const { id } = req.params;
+  try {
+    const result = await Contact.findByIdAndDelete(id);
+    if (result === null) {
+      return res.status(404).send({ message: "Not found" });
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
-export const createContact = (req, res) => {
+export async function createContact(req, res) {
   const bodyContact = {
     name: req.body.name,
     email: req.body.email,
     phone: req.body.phone,
+    favorite: req.body.favorite,
   };
-  const { error } = createContactSchema.validate(bodyContact, {
-    abortEarly: false,
-  });
-  if (error) {
-    return res
-      .status(400)
-      .json(error.details.map((error) => error.message).join(", "));
+  try {
+    const result = await Contact.create(bodyContact);
+    console.log(result);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  addContact(req.body)
-    .then((contact) => {
-      res.status(201).json(contact);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err.message });
+}
+
+export async function updateContact(req, res) {
+  const { id } = req.params;
+  try {
+    const bodyContact = {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      favorite: req.body.favorite,
+    };
+    const result = await Contact.findByIdAndUpdate(id, bodyContact, {
+      new: true,
     });
-};
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
-export const updateContact = (req, res) => {
-  const id = req.params.id;
-  const contact = req.body;
+export async function updateStatusContact(req, res) {
+  const { id } = req.params;
+  const { favorite } = req.body;
 
-  if (!Object.keys(contact).length) {
-    return res
-      .status(400)
-      .json({ message: "Body must have at least one field" });
+  if (typeof favorite !== "boolean") {
+    return res.status(400).json({ message: "Favorite must be a boolean" });
   }
 
-  const { error } = updateContactSchema.validate(contact, {
-    abortEarly: false,
-  });
-
-  if (error) {
-    return res.status(400).json({ message: error.message });
-  }
-
-  updContact(id, contact)
-    .then((updatedContact) => {
-      if (updatedContact) {
-        res.status(200).json(updatedContact);
-      } else {
-        res.status(404).json({ message: "Not found" });
+  try {
+    const updatedContact = await Contact.findByIdAndUpdate(
+      id,
+      { favorite },
+      {
+        new: true,
       }
-    })
-    .catch((err) => res.status(500).json({ message: err.message }));
-};
+    );
+
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,13 +1,63 @@
 import Joi from "joi";
+import mongoose from "mongoose";
 
-export const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.required(),
-  phone: Joi.string().required(),
+export const createContactSchema = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.required(),
+    phone: Joi.string().required(),
+    favorite: Joi.boolean(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
+};
+
+export const updateContactSchema = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string(),
+    phone: Joi.string(),
+    favorite: Joi.boolean(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
+};
+
+export const updateFavoriteSchema = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string(),
+    phone: Joi.string(),
+    favorite: Joi.boolean().required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
+};
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-export const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
-});
+export const Contact = mongoose.model("Contact", contactSchema);

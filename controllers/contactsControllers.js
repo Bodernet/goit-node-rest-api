5f98b8b2c96b2ce36025de040743283681847f18
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-05-27 14:30:47 +0300

    end 2

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,9 +2,12 @@ import { Contact } from "../schemas/contactsSchemas.js";
 
 export async function getAllContacts(req, res) {
   try {
-    const { page = 1, limit = 20 } = req.query;
-    const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner: req.user.id }, { skip, limit });
+    // const { page = 1, limit = 20 } = req.query;
+    // const skip = (page - 1) * limit;
+    const result = await Contact.find(
+      { owner: req.user.id }
+      // {skip, limit}
+    );
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -14,7 +17,7 @@ export async function getAllContacts(req, res) {
 export async function getOneContact(req, res) {
   const { id } = req.params;
   try {
-    const result = await Contact.findById({ _id: id, owner: req.user.id });
+    const result = await Contact.findOne({ _id: id, owner: req.user.id });
     if (result === null) {
       return res.status(404).send({ message: "Not found" });
     }
@@ -27,7 +30,7 @@ export async function getOneContact(req, res) {
 export async function deleteContact(req, res) {
   const { id } = req.params;
   try {
-    const result = await Contact.findByIdAndDelete({
+    const result = await Contact.findOneAndDelete({
       _id: id,
       owner: req.user.id,
     });
@@ -65,7 +68,7 @@ export async function updateContact(req, res) {
       phone: req.body.phone,
       favorite: req.body.favorite,
     };
-    const result = await Contact.findByIdAndUpdate(
+    const result = await Contact.findOneAndUpdate(
       { _id: id, owner: req.user.id },
       bodyContact,
       {
